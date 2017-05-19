f0dfc0505946ca80388db851e5d60f42f9ca0de9
Lauréline Guérin <zebuline@crocobox.org>
2017-05-19 14:16:52 +0200

    fix settings

--- demo/demoproject/settings.py
+++ demo/demoproject/settings.py
@@ -23,6 +23,27 @@ DATABASES = {
 }
 
 
+# Template.
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug': True,
+            'context_processors': (
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                'django.template.context_processors.request',
+                "django.contrib.messages.context_processors.messages"),
+        }
+    },
+]
+
+
 # Required.
 SECRET_KEY = "This is a secret made public on project's repository."
 
@@ -61,16 +82,5 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    'django.core.context_processors.request',
-    "django.contrib.messages.context_processors.messages")
-
 # Development configuration.
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
