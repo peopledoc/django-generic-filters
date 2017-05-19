e81074872c6623ec84e1f30721cab4cb64d102ea
Lauréline Guérin <zebuline@crocobox.org>
2017-05-19 14:16:23 +0200

    fix urls

--- demo/demoproject/filter/urls.py
+++ demo/demoproject/filter/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from demoproject.filter import views
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', views.user_list_view, name='user_filter_view'),
-)
+]
