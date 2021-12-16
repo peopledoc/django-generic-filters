091fac6e4c12e49967f8cd9893dd54aacb5ffcce
Joachim Jablon <joachim.jablon@people-doc.com>
2021-12-16 17:45:23 +0100

    adding GitHub Actions

--- setup.py
+++ setup.py
@@ -9,36 +9,43 @@ def read_relative_file(filename):
         return f.read().strip()
 
 
-NAME = 'django-generic-filters'
-README = read_relative_file('README.rst')
-VERSION = read_relative_file('VERSION')
-PACKAGES = ['django_genericfilters']
+NAME = "django-generic-filters"
+README = read_relative_file("README.rst")
+VERSION = read_relative_file("VERSION")
+PACKAGES = ["django_genericfilters"]
 REQUIRES = [
-    'Django',
-    'munch',
+    "Django",
+    "munch",
 ]
 
 
-if __name__ == '__main__':  # Don't run setup() when we import this module.
-    setup(name=NAME,
-          version=VERSION,
-          description='Easy filters for your Generic ListView with Django.',
-          long_description=README,
-          classifiers=['Development Status :: 4 - Beta',
-                       'License :: OSI Approved :: BSD License',
-                       'Programming Language :: Python :: 3',
-                       'Programming Language :: Python :: 3.6',
-                       'Programming Language :: Python :: 3.7',
-                       'Programming Language :: Python :: 3.8',
-                       'Framework :: Django',
-                       ],
-          keywords='class-based view, generic view, filters',
-          author='Novapost Team',
-          author_email='peopleask@novapost.fr',
-          url='https://github.com/novapost/%s' % NAME,
-          license='BSD',
-          packages=PACKAGES,
-          include_package_data=True,
-          install_requires=REQUIRES,
-          setup_requires=['setuptools'],
-          )
+if __name__ == "__main__":  # Don't run setup() when we import this module.
+    setup(
+        name=NAME,
+        use_scm_version=True,
+        description="Easy filters for your Generic ListView with Django.",
+        long_description=README,
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "License :: OSI Approved :: BSD License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Framework :: Django :: 2.2",
+            "Framework :: Django :: 3.0",
+            "Framework :: Django :: 3.1",
+            "Framework :: Django :: 3.2",
+            "Framework :: Django :: 4.0",
+        ],
+        keywords="class-based view, generic view, filters",
+        author="Novapost Team",
+        author_email="peopleask@novapost.fr",
+        url="https://github.com/novapost/%s" % NAME,
+        license="BSD",
+        packages=PACKAGES,
+        include_package_data=True,
+        install_requires=REQUIRES,
+        setup_requires=["setuptools"],
+    )
