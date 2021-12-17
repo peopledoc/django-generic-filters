b7b042d2b0bb14bf97420f61552be09ea63ba4ad
Joachim Jablon <joachim.jablon@people-doc.com>
2021-12-17 17:29:30 +0100

    Fix url() > path()

--- demoproject/filter/urls.py
+++ demoproject/filter/urls.py
@@ -1,6 +1,6 @@
 from demoproject.filter import views
-from django.conf.urls import url
+from django.urls import path
 
 urlpatterns = [
-    url(r"^$", views.user_list_view, name="user_filter_view"),
+    path(r"", views.user_list_view, name="user_filter_view"),
 ]
--- demoproject/urls.py
+++ demoproject/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 home = TemplateView.as_view(template_name="home.html")
 
 
 urlpatterns = [
-    url(r"^filter/", include("demoproject.filter.urls")),
+    path(r"filter/", include("demoproject.filter.urls")),
     # An informative homepage.
-    url(r"", home, name="home"),
+    path(r"", home, name="home"),
 ]
